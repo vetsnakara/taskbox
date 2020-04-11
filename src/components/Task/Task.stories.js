d6d987c1556e7372421afdebafe514a526c4e98f
Konstantin Arakantsev <kostya3312@gmail.com>
2020-04-11 16:10:12 +0300

    add and integrate knobs addon

--- src/components/Task/Task.stories.js
+++ src/components/Task/Task.stories.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
+import { withKnobs, object } from '@storybook/addon-knobs'
 
 import Task from './Task'
 
 export default {
   component: Task,
   title: 'Task',
-  excludeStories: /.*Data$/
+  excludeStories: /.*Data$/,
+  decorators: [withKnobs]
 }
 
 export const taskData = {
@@ -21,8 +23,48 @@ export const actionsData = {
   onArchiveTask: action('onArchiveTask')
 }
 
-export const Default = () => <Task task={{...taskData}} {...actionsData}/>
+const veryLontTitle = Array
+  .from(Array(20).keys())
+  .map(() => "Very long string.").join(" ")
 
-export const Pinned = () => <Task task={{...taskData, state: 'TASK_PINNED'}} {...actionsData}/>
+// Default
+export const Default = () => (
+  <Task
+    task={object('task', {...taskData})}
+    {...actionsData}
+  />
+)
 
-export const Archived = () => <Task task={{...taskData, state: 'TASK_ARCHIVED'}} {...actionsData}/>
+// Long title
+export const LongTitle = () => (
+  <Task
+    task={{
+      ...taskData, 
+      title: veryLontTitle, 
+      state: 'TASK_ARCHIVED'
+    }}
+    {...actionsData}
+  />
+)
+
+// Pinned task
+export const Pinned = () => (
+  <Task
+    task={{
+      ...taskData,
+      state: 'TASK_PINNED'
+    }}
+    {...actionsData}
+  />
+)
+
+// Archived task
+export const Archived = () => (
+  <Task
+    task={{
+      ...taskData,
+      state: 'TASK_ARCHIVED'
+    }}
+    {...actionsData}
+  />
+)
