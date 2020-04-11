3df3334215f89446f8e7a268da81482984801c68
Konstantin Arakantsev <kostya3312@gmail.com>
2020-04-11 15:13:58 +0300

    use a redux-mock decorator in `InboxScreen` stories

---
+++ src/components/InboxScreen/InboxScreen.stories.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import { Provider } from 'react-redux'
+
+import { PureInboxScreen } from './InboxScreen'
+import { defaultTasksData } from '../TaskList/TaskList.stories'
+
+export default {
+  component: PureInboxScreen,
+  title: 'InboxScreen',
+  decorators: [
+    story => <Provider store={store}>{story()}</Provider>
+  ]
+}
+
+const store = {
+  getState: () => {
+    return {
+      tasks: defaultTasksData
+    }
+  },
+  subscribe: () => null,
+  dispatch: action('dispatch')
+}
+
+export const Default = () => <PureInboxScreen/>
+
+export const Error = () => <PureInboxScreen error="Something"/>
